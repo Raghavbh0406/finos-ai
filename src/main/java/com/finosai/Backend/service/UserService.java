c5bf3d0beb10df9d104aa970da7eb9f2ab5a4ac0
Raghav Bhalla <269125996+Raghavbh0406@users.noreply.github.com.>
2026-06-08 01:51:11 +0530

    Fix forgot password implementation

--- src/main/java/com/finosai/Backend/service/UserService.java
+++ src/main/java/com/finosai/Backend/service/UserService.java
@@ -6,20 +6,26 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class UserService {
 
-
 private final UserRepository userRepository;
+private final EmailService emailService;
 
 private final PasswordEncoder passwordEncoder =
         new BCryptPasswordEncoder();
 
 public UserService(
-        UserRepository userRepository) {
+        UserRepository userRepository,
+        EmailService emailService) {
 
     this.userRepository =
             userRepository;
+
+    this.emailService =
+            emailService;
 }
 
 public User registerUser(
@@ -131,5 +137,91 @@ public void changePassword(
     );
 }
 
+public void forgotPassword(
+        String email) {
 
+    User user =
+            userRepository.findByEmail(
+                    email
+            );
+
+    if (user == null) {
+
+        throw new RuntimeException(
+                "User not found"
+        );
+    }
+
+    String token =
+            UUID.randomUUID()
+                    .toString();
+
+    user.setResetToken(
+            token
+    );
+
+    userRepository.save(
+            user
+    );
+
+    String resetLink =
+            "https://finos-ai-production.up.railway.app/reset-password-page?token="
+                    + token;
+
+    emailService.sendPasswordResetEmail(
+            user.getEmail(),
+            resetLink
+    );
+}
+
+public void resetPassword(
+        String token,
+        String newPassword) {
+
+    User user =
+            userRepository.findByResetToken(
+                    token
+            );
+
+    if (user == null) {
+
+        throw new RuntimeException(
+                "Invalid token"
+        );
+    }
+
+    user.setPassword(
+            passwordEncoder.encode(
+                    newPassword
+            )
+    );
+
+    user.setResetToken(
+            null
+    );
+
+    userRepository.save(
+            user
+    );
+}
+public void deleteUser(
+        String email) {
+
+    User user =
+            userRepository.findByEmail(
+                    email
+            );
+
+    if (user == null) {
+
+        throw new RuntimeException(
+                "User not found"
+        );
+    }
+
+    userRepository.delete(
+            user
+    );
+
+}
 }
