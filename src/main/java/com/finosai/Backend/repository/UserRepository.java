9e68e3bd285bc732b0ad10f266ed4abffbdd121a
Raghav Bhalla <269125996+Raghavbh0406@users.noreply.github.com.>
2026-06-10 01:42:59 +0530

    Remove findByResetToken from UserRepository

--- src/main/java/com/finosai/Backend/repository/UserRepository.java
+++ src/main/java/com/finosai/Backend/repository/UserRepository.java
@@ -3,17 +3,6 @@ package com.finosai.Backend.repository;
 import com.finosai.Backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository
-extends JpaRepository<User, Long> {
-
-
-User findByEmail(
-        String email
-);
-
-User findByResetToken(
-        String resetToken
-);
-
-
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
